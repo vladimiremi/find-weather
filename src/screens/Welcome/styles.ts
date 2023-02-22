3b756c0998e59d80a2b0c64df181139cac45d771
vladimiremi <vladimiremi2014@gmail.com>
2023-02-22 10:02:34 -0300

    refactor: improvement Welcome screen

--- src/screens/Welcome/styles.ts
+++ src/screens/Welcome/styles.ts
@@ -3,27 +3,10 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.DARK};
-  padding: 16px;
-`;
-
-export const Title = styled.Text`
-  margin-top: 57px;
-  color: ${({ theme }) => theme.COLORS.WHITE};
-  font-size: 33px;
-  text-align: center;
-  font-family: ${({ theme }) => theme.FONT_FAMILY.SEMI_BOLD};
-`;
-
-export const Subtitle = styled.Text`
-  margin-top: 33px;
-  color: ${({ theme }) => theme.COLORS.GRAY_100};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  text-align: center;
+  padding: 0 16px;
 `;
 
 export const ContainerLogo = styled.View`
-  margin-top: 137px;
   align-self: center;
 `;
 
