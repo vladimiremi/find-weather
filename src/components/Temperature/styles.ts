71caf51fd225b9bb06d47337fc7168f9f963c920
vladimiremi <vladimiremi2014@gmail.com>
2023-02-26 14:26:19 -0300

    refactor: improve temperature component

--- src/components/Temperature/styles.ts
+++ src/components/Temperature/styles.ts
@@ -2,6 +2,17 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex-direction: row;
-  align-items: flex-start;
+  align-items: center;
   justify-content: center;
+  height: 100px;
+`;
+
+export const MaxTemperatureContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const MinTemperatureContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
 `;
