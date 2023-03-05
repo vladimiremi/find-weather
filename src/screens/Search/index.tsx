2c6e7daebf425bb2501b788159aa1b06efa59455
vladimiremi <vladimiremi2014@gmail.com>
2023-03-05 10:58:08 -0300

    refactor: integration with FindWeatherAPI and improvement screen

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTheme } from 'styled-components/native';
 import Divider from '../../components/Divider';
 import { Header } from '../../components/Header';
-import { Container } from './styles';
+import {
+  Container,
+  ContainerIconLocation,
+  ContainerIconSearch,
+  ContainerSearch,
+} from './styles';
 import InputSearch from '../../components/InputSearch';
-import CityBox from '../../components/CityBox';
+import CityBox, { ICityDataProps } from '../../components/CityBox';
 import Empty from '../../components/Empty';
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+
+import { FindWeatherAPI } from '../../services/FindWeatherAPI';
 
 function Search() {
   const theme = useTheme();
+  const [search, useSearch] = useState('');
+  const [cities, setCities] = useState<ICityDataProps[]>();
+  const handleSearchCity = async (city: string) => {
+    try {
+      const resp = await FindWeatherAPI.getSearch(city);
+      setCities(resp.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Container>
       <Divider top={52} />
@@ -17,10 +36,22 @@ function Search() {
 
       <Divider top={48} />
 
-      <InputSearch />
+      <ContainerSearch>
+        <ContainerIconSearch>
+          <AntDesign name="search1" size={24} color={theme.COLORS.WHITE} />
+        </ContainerIconSearch>
+        <InputSearch value={search} onChangeText={value => useSearch(value)} />
+        <ContainerIconLocation onPress={() => handleSearchCity(search)}>
+          <Ionicons
+            name="ios-location-sharp"
+            size={30}
+            color={theme.COLORS.WHITE}
+          />
+        </ContainerIconLocation>
+      </ContainerSearch>
 
       <Divider top={42} />
-      {true ? <CityBox /> : <Empty />}
+      {!!cities?.length ? <CityBox data={cities} /> : <Empty />}
     </Container>
   );
 }
--- src/screens/Search/styles.ts
+++ src/screens/Search/styles.ts
@@ -1,8 +1,33 @@
-import styled from "styled-components/native";
-
+import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.DARK};
   padding: 0 16px;
+`;
+
+export const ContainerSearch = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const ContainerIconSearch = styled.View`
+  position: absolute;
+  z-index: 1;
+  left: 11px;
+`;
+
+export const ContainerIconLocation = styled.TouchableOpacity`
+  margin-left: 16px;
+  width: 15%;
+  height: 100%;
+  padding: 9px;
+  flex-direction: row;
+  justify-content: center;
+
+  align-items: center;
+  justify-self: center;
+  border-radius: 11px;
+  background-color: ${({ theme }) => theme.COLORS.DARK_400};
 `;
