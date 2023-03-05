57d125fbfa6fa28a600c3c557e6d18a8e6e943bf
vladimiremi <vladimiremi2014@gmail.com>
2023-03-05 10:51:56 -0300

    refactor: add new react navigation properties

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -2,8 +2,11 @@ export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       home: undefined;
+      search: undefined;
       welcome: undefined;
-      'home/moredays': undefined;
+      'home/moredays': {
+        city: string;
+      };
     }
   }
 }
