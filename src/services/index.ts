e47e60347c6855bf74a770720dc5e791a86b8129
vladimiremi <vladimiremi2014@gmail.com>
2023-03-05 07:29:27 -0300

    feat: config axios

---
+++ src/services/index.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://api.weatherapi.com/v1',
+  timeout: 1000,
+  params: {
+    key: process.env.KEY_WEATHERAPI,
+  },
+  headers: { 'X-Custom-Header': 'foobar' },
+});
+
+export default api;
