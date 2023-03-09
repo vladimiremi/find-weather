474776b0cad1a2327d6688e7791b440202e337ee
vladimiremi <vladimiremi2014@gmail.com>
2023-03-09 07:47:52 -0300

    refactor: add getHourAndMinute

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -4,13 +4,20 @@ import 'intl/locale-data/jsonp/pt-BR';
 
 export const formatDate = (dateString) => {
   const data = new Date(dateString);
-  const formatoData = new Intl.DateTimeFormat('pt-BR', {
+  const formatData = new Intl.DateTimeFormat('pt-BR', {
     weekday: 'long',
     day: 'numeric',
     month: 'long',
     year: 'numeric',
   });
-  const dataFormatada = formatoData.format(data);
-  const dataFinal = dataFormatada.replace('de', '');
+  const dataFormatted = formatData.format(data);
+  const dataFinal = dataFormatted.replace('de', '');
   return dataFinal;
 };
+
+export const getHourAndMinute = (dateString:string)=> {
+
+  const hourAndMinute = dateString.split(" ")[1];
+
+  return hourAndMinute
+}
