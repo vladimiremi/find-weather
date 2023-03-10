b47e994667e45258ae21f4d256851aa2d4152e8c
vladimiremi <vladimiremi2014@gmail.com>
2023-03-10 20:17:23 -0300

    refactor: check if user already did first access

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,22 @@ import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './app.routes';
 import { useTheme } from 'styled-components/native';
-import { InitialRoutes } from './initital.routes';
+import { InitialRoutes } from './welcome.routes';
+import { userStatus } from '../context/userContext';
+
 
 export default function Routes() {
   const { COLORS } = useTheme();
+
+  const { firstAccess } = userStatus()
   return (
     <View style={{ flex: 1, backgroundColor: COLORS.DARK }}>
       <NavigationContainer>
-        {/* <InitialRoutes /> */}
-        <AppRoutes />
+        {
+          firstAccess ? <AppRoutes /> : <InitialRoutes />
+        }
+
+
       </NavigationContainer>
     </View>
   );
