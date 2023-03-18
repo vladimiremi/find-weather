f606435151f8cb2cede5e2c695e6de802312f130
vladimiremi <vladimiremi2014@gmail.com>
2023-03-18 08:50:50 -0300

    refactor: rename methods from FindWeatherAPI

--- src/services/FindWeatherAPI.ts
+++ src/services/FindWeatherAPI.ts
@@ -1,13 +1,13 @@
-import { api } from './api';
+import { apiWeather } from './api';
 
 export const FindWeatherAPI = {
   getForecast: (city: string) => {
-    return api.get(`forecast.json?q=${city}&days=1&aqi=no&alerts=no&lang=pt`);
+    return apiWeather.get(`forecast.json?q=${city}&days=1&aqi=no&alerts=no&lang=pt`);
   },
   getCurrentWeather: (city: string)=> {
-    return api.get(`current.json?q=${city}&days=1&aqi=no&alerts=no&lang=pt`)
+    return apiWeather.get(`current.json?q=${city}&days=1&aqi=no&alerts=no&lang=pt`)
   },
   getSearch: (city: string) => {
-    return api.get(`search.json?q=${city}&days=1&aqi=no&alerts=no&lang=pt`);
+    return apiWeather.get(`search.json?q=${city}&days=1&aqi=no&alerts=no&lang=pt`);
   },
 };
