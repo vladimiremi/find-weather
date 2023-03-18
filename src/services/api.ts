2b1525296428948eba5d40a4d53b255d2046d290
vladimiremi <vladimiremi2014@gmail.com>
2023-03-18 08:49:53 -0300

    feat: add service config with open weather map

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,24 @@
 import axios from 'axios';
 
-const api = axios.create({
+const apiWeather = axios.create({
   baseURL: 'http://api.weatherapi.com/v1',
   timeout: 1000,
   params: {
     key: process.env.KEY_WEATHERAPI,
   },
-  headers: { 'X-Custom-Header': 'foobar' },
 });
 
-export { api };
+const apiOpenWeatherMap = axios.create({
+  baseURL: 'https://api.openweathermap.org/data/2.5',
+  params: {
+    appid: process.env.KEY_OPENWEATHERMAP
+  }
+})
+const apiOpenWeatherMapGeo = axios.create({
+  baseURL: 'http://api.openweathermap.org/geo/1.0',
+  params: {
+    appid: process.env.KEY_OPENWEATHERMAP
+  }
+})
+
+export { apiWeather, apiOpenWeatherMap, apiOpenWeatherMapGeo };
