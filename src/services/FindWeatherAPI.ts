e08e78d54f582c7e34e6e8917acf6d22c39f5f2b
Vladimir Costa <41305527+vladimiremi@users.noreply.github.com>
2023-03-19 06:01:13 -0300

    refactor: remove unsed servise

--- src/services/FindWeatherAPI.ts
+++ src/services/FindWeatherAPI.ts
@@ -2,12 +2,6 @@ import { apiWeather } from './api';
 
 export const FindWeatherAPI = {
   getForecast: (city: string) => {
-    return apiWeather.get(`forecast.json?q=${city}&days=1&aqi=no&alerts=no&lang=pt`);
-  },
-  getCurrentWeather: (city: string)=> {
-    return apiWeather.get(`current.json?q=${city}&days=1&aqi=no&alerts=no&lang=pt`)
-  },
-  getSearch: (city: string) => {
-    return apiWeather.get(`search.json?q=${city}&days=1&aqi=no&alerts=no&lang=pt`);
+    return apiWeather.get(`forecast.json?key=${process.env.KEY_WEATHERAPI}&q=${city}&days=1&aqi=no&alerts=no&lang=pt`);
   },
 };
